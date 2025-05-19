0a33a54ff345e0625845a7ce5e5750aaddeee9cc
nessa <yisel7948@gmail.com>
2025-05-19 09:31:20 -0500

    nuevo todo

---
+++ app/src/main/java/com/example/sstdoble/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sstdoble;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText etCorreo, etPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etCorreo = findViewById(R.id.etCorreo);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sstdoble/Menu.java
@@ -0,0 +1,25 @@
+package com.example.sstdoble;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import com.example.sstdoble.Menu;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Menu extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+    }
+}
